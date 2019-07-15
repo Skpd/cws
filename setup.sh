126eaa6b8e759310daf2b7683ea62f54d3f09d86
Dmitry Shabanov <dshabanov@threatstop.com>
2019-07-15 11:19:47 -0700

    updated requirements, setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-./venv/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/client/client.proto
-./venv/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/stream/stream.proto
-./venv/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/markets/asset.proto
-./venv/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/markets/index.proto
-./venv/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/markets/pair.proto
-./venv/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/markets/market.proto
+VENV="./venv"
+# init virtual environment. use any python >= 3.6
+python3.7 -m venv ${VENV}
+
+# install python libraries
+${VENV}/bin/pip install -r requirements.txt
+
+# init proto submodule
+git submodule init
+git submodule update
+
+# install / compile protocols
+${VENV}/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/client/client.proto
+${VENV}/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/stream/stream.proto
+${VENV}/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/markets/asset.proto
+${VENV}/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/markets/index.proto
+${VENV}/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/markets/pair.proto
+${VENV}/bin/python -m grpc_tools.protoc -I ./proto --python_out ./ proto/markets/market.proto
